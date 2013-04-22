014ae288a92424113c77e16a0fcf45c70c08541d
Nikola Hardi <atlantic777@lugons.org>
2013-04-22 17:00:15 +0200

    Fixed some errors.

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
- from django.contrib import admin
- admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.foo.urls')),
-     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-     url(r'^admin/', include(admin.site.urls)),
-     url(r'^$', 'home.views.home'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', 'home.views.home'),
 )

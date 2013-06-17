733a065498279282ceb1962e62b7f523f2d318ee
Nikola Hardi <atlantic777@lugons.org>
2013-06-17 23:08:04 +0200

    Cleanup up to blog

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 def home(request):
-	return render_to_response("home.html")
+	return redirect('/blog/')
